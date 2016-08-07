7d83abff14314b9a636c068ccfe1c49255728186
Adam Lombard <willcodeforgin@gmail.com>
2016-08-06 23:29:54 -0700

    Exercises for ch3 -- Variables

---
+++ 3-variables/name.rb
@@ -0,0 +1,9 @@
+puts "Please enter your first name:"
+fname = gets.chomp
+
+puts "Nice! Please enter your last name:"
+lname = gets.chomp
+
+puts "Welcome, #{fname} #{lname}!"
+
+10.times { puts "#{fname} #{lname}" }
